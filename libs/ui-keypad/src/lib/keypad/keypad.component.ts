2e4a88f1e0cefc560a3c5d76e4497cf6667820cf
Damian Falana <falanadamian@gmail.com>
2019-07-08 21:26:35 +0200

    Implemented keypad component

--- libs/ui-keypad/src/lib/keypad/keypad.component.ts
+++ libs/ui-keypad/src/lib/keypad/keypad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'ui-keypad',
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeypadComponent implements OnInit {
 
-  constructor() { }
+  @Input() rangeStart: number = 1;
+  @Input() rangeEnd: number = 9;
+  @Input() keypadColumns: number = 3;
+  @Input() resetTime: number = 2000;
+
+  @Output() onKeypadChange: EventEmitter<string> = new EventEmitter<string>();
+
+  digits: string;
+
+  rows: Array<number>;
+  keys: Array<number>;
+
+  private timeout: NodeJS.Timer;
+
+  constructor() {
+    this.resetInput();
+  }
+
+  resetInput() {
+    clearTimeout(this.timeout);
+    this.digits = '';
+  }
+
+  private setTimeout(): NodeJS.Timer {
+    return setTimeout(() => {
+      this.onKeypadChange.emit(this.digits);
+      this.resetInput();
+    }, this.resetTime);
+  }
+
+  private manageTimeout() {
+    clearTimeout(this.timeout);
+    this.timeout = this.setTimeout();
+  }
+
+  onKeyClick(key: number) {
+    this.manageTimeout();
+    this.digits += key;
+  }
+
+  undoInput() {
+    this.digits = this.digits.slice(0, -1);
+    this.digits ? this.manageTimeout() : clearTimeout(this.timeout);
+  }
+
+  private numbersBetween(start: number, end: number): Array<number> {
+    const arraySize: number = Math.floor(end) - Math.ceil(start) + 1;
+    return Array(arraySize).fill(1)
+                            .map((x: number, index: number) => Math.ceil(start) + index);
+  }
+
+  private initializeKeys() {
+    this.keys = this.numbersBetween(this.rangeStart, this.rangeEnd);
+  }
+
+  private initializeRows() {
+    const rowNumbers = Math.ceil(this.keys.length / this.keypadColumns - 1);
+    this.rows = this.numbersBetween(0, rowNumbers > 0 ? rowNumbers : 1);
+  }
 
   ngOnInit() {
+    this.initializeKeys();
+    this.initializeRows();
   }
 
 }
--- libs/ui-keypad/src/lib/ui-keypad.module.ts
+++ libs/ui-keypad/src/lib/ui-keypad.module.ts
@@ -1,9 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { KeypadComponent } from './keypad/keypad.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {KeypadComponent} from './keypad/keypad.component';
+import {FormsModule} from "@angular/forms";
+import {AngularMaterialModule} from "@parking-ticketing-system/angular-material";
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [KeypadComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    AngularMaterialModule
+  ],
+  declarations: [
+    KeypadComponent
+  ],
+  exports: [
+    KeypadComponent
+  ]
 })
-export class UiKeypadModule {}
+export class UiKeypadModule {
+}
