75f173f8b62ce96ab1c3a2423a965ffb831cc2c6
Damian Falana <falanadamian@gmail.com>
2019-07-09 11:11:15 +0200

    Added application layout

--- apps/parking-ticketing-system/src/app/app.component.ts
+++ apps/parking-ticketing-system/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'parking-ticketing-system-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'parking-ticketing-system';
+export class AppComponent implements OnInit {
+
+  private readonly SIDENAV_DISPLAY_BREAKPOINT: number = 600;
+
+  @ViewChild('sidenav', {static: false}) sidenav;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event) {
+    this.windowInnerWidth = event.target.innerWidth;
+    if(!this.shouldDisplaySidenav(this.windowInnerWidth)) {
+      this.sidenav.close();
+    }
+  }
+
+  private windowInnerWidth: number;
+
+  sidenavOpened: boolean = false;
+
+  title = 'Parking ticketing system';
+
+  links = [
+    { path: '/ticket-machine', icon: 'directions_car', title: 'Ticket machine'}
+  ];
+
+  private shouldDisplaySidenav(screenWidth: number): boolean {
+    return screenWidth > this.SIDENAV_DISPLAY_BREAKPOINT;
+  }
+
+  toggleSidenav() {
+    if(this.shouldDisplaySidenav(this.windowInnerWidth)) {
+      this.sidenav.toggle();
+    }
+  }
+
+  ngOnInit() {
+    this.windowInnerWidth = window.innerWidth;
+    if(this.shouldDisplaySidenav(this.windowInnerWidth)){
+      this.sidenavOpened = true;
+    }
+  }
+
 }
--- apps/parking-ticketing-system/src/app/app.module.ts
+++ apps/parking-ticketing-system/src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularMaterialModule} from "@parking-system/angular-material";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
