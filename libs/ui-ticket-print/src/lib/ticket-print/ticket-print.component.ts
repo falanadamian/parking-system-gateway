a4de03297330c7a44c159c07a96ee2e8d3fc8e85
Damian Falana <falanadamian@gmail.com>
2019-07-09 12:21:58 +0200

    Created ticket print component

---
+++ libs/ui-ticket-print/src/lib/ticket-print/ticket-print.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-ticket-print',
+  templateUrl: './ticket-print.component.html',
+  styleUrls: ['./ticket-print.component.scss']
+})
+export class TicketPrintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/ui-ticket-print/src/lib/ui-ticket-print.module.ts
+++ libs/ui-ticket-print/src/lib/ui-ticket-print.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TicketPrintComponent } from './ticket-print/ticket-print.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [TicketPrintComponent]
 })
 export class UiTicketPrintModule {}
