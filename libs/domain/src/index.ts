9c92fa3d729d962a1f8dc4f3e61a00bec67d263d
Damian Falana <falanadamian@gmail.com>
2019-07-09 12:09:43 +0200

    Created keypad template

--- libs/domain/src/index.ts
+++ libs/domain/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/domain.module';
 export { MachineState } from './lib/machine/machine-state.enum'
 export { CarStop } from './lib/carstop/car-stop'
+export { KeypadTemplate } from './lib/templates/keypad/keypad-template'
